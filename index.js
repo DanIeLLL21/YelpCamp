a42ad4313213e42a1b0ebcd9067c0c4e3beeb631
Daniel <daniel.persic32@gmail.com>
2021-09-17 15:33:32 +0200

    add start script

--- index.js
+++ index.js
@@ -139,9 +139,12 @@ app.use((err,req,res,next)=>{
 
 })
 
-app.listen(3000,()=>{
 
-console.log("serving on port3000")
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+
+console.log(`Serving on ${port}`)
 })
 
 
